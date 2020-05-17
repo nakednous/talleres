37e60579bc2c08b88eba3782c66df470e00fe819
nakednous <nakednous@gmail.com>
2020-05-17 17:23:19 -0500

    std style applied

--- docs/js/sketch.js
+++ docs/js/sketch.js
@@ -1,33 +1,33 @@
-var shapes = [];
-var grid = true;
+var shapes = []
+var grid = true
 
-function setup() {
-  createCanvas(400, 400);
+function setup () {
+  createCanvas(400, 400)
   for (let i = 0; i < 7; i++) {
-    shapes[i] = new Shape();
+    shapes[i] = new Shape()
   }
 }
 
-function draw() {
-  background(180);
+function draw () {
+  background(180)
   if (grid) {
-    gridHint(10);
+    gridHint(10)
   }
   for (var i = 0; i < shapes.length; i++) {
-    shapes[i].draw();
+    shapes[i].draw()
   }
 }
 
-function gridHint(scale) {
-  push();
-  stroke(0, 255, 255);
-  strokeWeight(1);
-  let i;
-  for (i=0; i<=width/scale; i++) {
-    line(i*scale, 0, i*scale, height);
+function gridHint (scale) {
+  push()
+  stroke(0, 255, 255)
+  strokeWeight(1)
+  let i
+  for (i = 0; i <= width / scale; i++) {
+    line(i * scale, 0, i * scale, height)
   }
-  for (i=0; i<=height/scale; i++) {
-    line(0, i*scale, width, i*scale);
+  for (i = 0; i <= height / scale; i++) {
+    line(0, i * scale, width, i * scale)
   }
-  pop();
+  pop()
 }
