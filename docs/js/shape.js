ad955612a8e2f3ccb5eb6b9853438cbab4c45f71
nakednous <nakednous@gmail.com>
2020-05-20 13:44:44 -0500

    shape getters and setters added

--- docs/js/shape.js
+++ docs/js/shape.js
@@ -1,13 +1,13 @@
 class Shape {
+  
   constructor () {
-    this.tinte = color(random(0, 255), random(0, 255), random(0, 255));
-    this.dim = random(50, 200);
+    this.hue = color(random(0, 255), random(0, 255), random(0, 255));
     this.posicion = createVector(random(0, width), random(0, height));
   }
 
   draw () {
     push();
-    fill(this.tinte);
+    fill(this.hue);
     translate(this.posicion.x, this.posicion.y);
     this.drawShape();
     pop();
@@ -17,5 +17,40 @@ class Shape {
   // https://medium.com/@yuribett/javascript-abstract-method-with-es6-5dbea4b00027
   drawShape() {
     throw new Error('You have to implement the method drawShape!');
+  }
+
+  // getters and setter
+  // see: https://www.w3schools.com/js/js_object_accessors.asp
+
+  get scaling() {
+    return this._scl;
+  }
+
+  set scaling(s) {
+    this._scl = s;
+  }
+
+  get rotation() {
+    return this._rot;
+  }
+
+  set rotation(r) {
+    this._rot = r;
+  }
+
+  get position() {
+    return this._trans;
+  }
+
+  set position(t) {
+    this._trans = t;
+  }
+
+  get hue() {
+    return this._hue;
+  }
+
+  set hue(h) {
+    this._hue = h;
   }
 }
