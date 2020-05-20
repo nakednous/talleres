a942a97193423e2783387332f77184b54e0d6a71
nakednous <nakednous@gmail.com>
2020-05-20 13:15:10 -0500

    fix typo

--- docs/js/rect.js
+++ docs/js/rect.js
@@ -2,22 +2,18 @@ class Rect extends Shape {
   constructor () {
     super();
     this.edge = 100;
-    //this.setEdge(100);  
   }
 
   drawShape() {
     rectMode(CENTER);
-    //rect(0, 0, 100, 100);
-    rect(0, 0, this.edge(), this.edge());
+    rect(0, 0, edge(), edge());
   }
 
-  edge() {
+  get edge() {
     return this.edge;
   }
 
-  /*
-  setEdge(e) {
-    this.edge = e;
+  set edge(value) {
+    this.edge = value;
   }
-  */
 }
